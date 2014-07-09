e4d778769d9e386d69c9282be57c89cbf9b9bf85
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-07-09 12:47:22 -0500

    Add logging handlers
    Set up logging for CRABAPI as well as the CRAB3 client

--- CRABAPI/TopLevel.py
+++ CRABAPI/TopLevel.py
@@ -1,7 +1,31 @@
 """ Module storing top-level functions that are exported to the CRABAPI
     package. These functions can also be accessed from CRABAPI.<name>"""
 
+import logging
+
+API_LOGGER_NAME = 'CRAB3.CRABAPI'
+
 def getTask(taskName):
     """ Given a task name, initialize a task object"""
     taskName += "test"
     raise NotImplementedError("Need to implement this: Load %s" % taskName)
+
+def setLogging(apiLevel = logging.INFO,
+                crabLevel = 100,
+                crabTracebackLevel = 100):
+    """Set logging parameters. Mutes the CRAB client by default.
+
+      returns apiLogger"""
+    crabLog = logging.getLogger('CRAB3')
+    crabTracebackLog = logging.getLogger('CRAB3:traceback')
+    apiLog = logging.getLogger(API_LOGGER_NAME)
+
+    apiLog.setLevel(apiLevel)
+    crabLog.setLevel(crabLevel)
+    crabTracebackLog.setLevel(crabTracebackLevel)
+
+    return apiLog
+
+def getLogger():
+    """ Helper function to get the logger back """
+    return logging.getLogger(API_LOGGER_NAME)
--- CRABAPI/__init__.py
+++ CRABAPI/__init__.py
@@ -2,6 +2,6 @@
     Normally I keep __init__.py blank, but this is a but of delicious
     syntactic sugar for some common operations"""
 
-from CRABAPI.TopLevel import getTask
+from CRABAPI.TopLevel import getTask, setLogging
 
-__all__ = ["getTask"]
+__all__ = ["getTask","setLogging"]
