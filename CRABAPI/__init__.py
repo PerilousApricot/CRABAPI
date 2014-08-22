394efeb674181e2f79b2e1184997ffdd6566f3cb
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-08-22 13:51:05 -0500

    Make sure logs are initialized before tests run

--- CRABAPI/__init__.py
+++ CRABAPI/__init__.py
@@ -5,3 +5,10 @@
 from CRABAPI.TopLevel import getTask, setLogging, getAllLoggers, getLogger
 
 __all__ = ["getTask", "setLogging", "getAllLoggers", "getLogger"]
+
+def setUpPackage():
+    """ Need to make sure logging is initialized before any tests run. This
+        should NOT be called by client functions, it is used by the testing
+        suite """
+    import logging
+    setLogging(logging.DEBUG, logging.DEBUG, logging.DEBUG)
