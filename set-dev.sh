af931e378876c79d0dffa675fcf5bcf36c7944ff
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-08-22 13:50:21 -0500

    Fixing logic for development environment

--- set-dev.sh
+++ set-dev.sh
@@ -1,5 +1,6 @@
 . venv/bin/activate
 export PYTHONPATH=WMCore/src/python:CRABClient/src/python:CRABServer/src/python:$PYTHONPATH
 cmsset
-cd $(ls CMSSW_*|head -n1)/src
+cd $(ls -d CMSSW_*|head -n1)/src
 cmsenv
+set -
